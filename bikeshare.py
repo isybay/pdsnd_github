d87890031d1d3b24b18e02bf1cc396410504ad66
Israel Ayodamola Bayode <isybay@gmail.com>
2021-11-11 13:37:41 +0100

    refractor: Added a print statement at the beginning of the code

--- bikeshare.py
+++ bikeshare.py
@@ -5,6 +5,8 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
+print("Are you ready?")
+
 
 def get_filters():
     """
